4fba2954753022c050f7f2cbf505193addbfd458
pinugit <pinuwork@gmail.com>
2024-08-13 10:30:51 +0530

    added article api

--- src/index.js
+++ src/index.js
@@ -1,42 +1,62 @@
-import express, { query } from "express";
+import express from "express";
+import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
 const app = express();
 
 const PORT = 5000;
 
-const users = [
-	{ id: 1, username: "user1", displayname: "User One" },
-	{ id: 2, username: "user2", displayname: "User Two" },
-	{ id: 3, username: "user3", displayname: "User Three" },
-	{ id: 4, username: "user4", displayname: "User Four" },
-	{ id: 5, username: "user5", displayname: "User Five" },
-	{ id: 6, username: "user6", displayname: "User Six" },
-	{ id: 7, username: "user7", displayname: "User Seven" },
-	{ id: 8, username: "user8", displayname: "User Eight" },
-];
-
 app.use(express.json());
 
-app.get("/", (req, res) => {
-	res.status(200).json({ msg: "server is working" });
-});
+app.get("/", (req, res) => {});
 
-app.get("/api/users", (req, res) => {
+app.get("/api/users", async (req, res) => {
 	const {
 		query: { filter, value },
 	} = req;
 
-	if (filter && value)
-		return res
-			.status(200)
-			.json(users.filter((user) => user[filter].includes(value)));
-
+	const users = await prisma.user.findMany();
 	res.status(200).json(users);
 });
 
-app.post("/api/users", (req, res) => {
-	console.log(req.body);
-	res.json({ msg: "less go" });
+app.post("/api/users", async (req, res) => {
+	try {
+		const { name, email } = req.body;
+		if (!name && !email)
+			return res.status(500).json({ msg: "enter some data" });
+
+		const user = await prisma.user.create({
+			data: {
+				name: name,
+				email: email,
+			},
+		});
+
+		return res.status(201).json({ user });
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ error: "error creating blog" });
+	}
+});
+
+app.get("/api/articles", async (req, res) => {
+	const articles = await prisma.article.findMany();
+
+	res.status(200).json(articles);
+});
+
+app.post("/api/articles/:userId", async (req, res) => {
+	const userId = Number.parseInt(req.params.userId);
+
+	const article = await prisma.article.create({
+		data: {
+			title: req.body.title,
+			body: req.body.body,
+			authorId: userId,
+		},
+	});
+
+	res.status(201).json(article);
 });
 
 app.listen(PORT, () => {
